c2a6556805dc6df9d9612799d610390020870a14
Kevin Chen <kcheart@gmail.com>
2020-07-02 16:16:09 +0800

    change LandmarkDetail, pass data

--- SwiftUI-workshop-1/LandmarkDetail.swift
+++ SwiftUI-workshop-1/LandmarkDetail.swift
@@ -9,25 +9,27 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
         
         VStack {
-            MapView(coordinate: landmarkData[0].locationCoordinate)
+            MapView(coordinate: landmark.locationCoordinate)
                 .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
 
-            CircleImage(image: Image("turtlerock"))
+            CircleImage(image: landmark.image)
                 .offset(x: 0, y: -130)
                 .padding(.bottom, -130)
 
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
                 HStack {
-                    Text("Joshua Tree National Park")
+                    Text(landmark.park)
                         .font(.subheadline)
                     Spacer()
-                    Text("Calfornia")
+                    Text(landmark.state)
                         .font(.subheadline)
                 }
             }
@@ -35,11 +37,12 @@ struct LandmarkDetail: View {
             
             Spacer()
         }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 struct LandmarkDetail_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkDetail()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }
